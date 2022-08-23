a0669de581ffe5ba78656963364e6cee55e909f2
Gilberto Garza <55161386+gilgarzadev@users.noreply.github.com>
2022-08-23 17:41:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+/* let nombre = prompt("Ingrese su Nombre");
+
+if(nombre === "gil"){
+    alert("Hola Gil");
+}else{
+    alert("Quien eres?");
+}
+
+alert("Terminamos"); */
+
+/* let terminaste = prompt("Terminaste tu tarea?");
+
+if(terminaste === "si"){
+    alert("Puedes salir a jugar");
+}else{
+    alert("no puedes salir a jugar");
+} */
+
+/* let edad = parseInt(prompt("Ingrese su edad"));
+
+if(edad < 15){
+    alert("No puedes entrar");
+}else if(edad < 18){
+    alert("Puede entrar con un adulto");
+}else if(edad > 35){
+    alert("ya estas muy viejo, regrese a casa!");
+}else{
+    alert("Puede entrar a la fiesta");
+} */
+    
+let terminaste = prompt("Terminaste la Univerdidad?");
+
+if(terminaste === "si"){
+    alert("Te felicito! Eres el mejor.");
+}else{
+    alert = prompt("Que estas esperando para terminar?");
+}
---
+++ index2.js
@@ -0,0 +1,28 @@
+
+let usuario = prompt("Identificate!, Cual es tu nombre?");
+    alert("Hola " + usuario); 
+let terminaste = prompt("Terminaste la Univerdidad?");
+if(terminaste === "si"){
+    alert("OK! Veamos..");
+}else{
+    alert("No puedes ingresar!")
+    /* alert = prompt("Que estas esperando para terminar?"); */
+}
+let prueba = prompt("Cual es el resultado de 2 + 2"); 
+   
+while(prueba != "4"){
+    alert("Respuesta incorrecta!");
+    alert("Vamos de nuevo!");
+    prueba = prompt("Cual es el resultado de 2 + 2");
+}
+
+alert("Correcto! Eres Bienvenid@.");
+
+
+
+
+/* if(terminaste === "si"){
+    alert("Te felicito! Eres el mejor.");
+}else{
+    alert = prompt("Que estas esperando para terminar?");
+} */
